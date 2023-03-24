89f22673f5f12abf461f506ed5d98504a8736fed
unkno <unknown14584@mail.ru>
2023-03-24 20:55:22 +0300

    Promises

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hey!");
+//console.log("Hey!");
+
+let promise_1 = new Promise(function(resolve, reject) {
+    let random_1 = Math.random();
+    if (random_1 > 0.5) {
+        resolve("Крутяк!");
+    } else {
+        reject("Непруха(");
+    }
+});
+
+let promise_2 = new Promise(function(resolve, reject) {
+    let random_2 = Math.random();
+    if (random_2 > 0.2) {
+        resolve("Чел, хорош!");
+    } else {
+        reject("Не хорош(");
+    }
+})
+
+promise_1
+.then(function(result) {
+    console.log(result);
+    return promise_2;
+})  
+
+.then(function(result2) {
+    console.log(result2);
+    console.log("На сегодня все)");
+})
+
+.catch(function(error) {
+    console.warn(error);
+})
+
+//console.log(promise_1);
